bc1b847043afd4c2f7a3b7f9dced8b90d60ba00f
Ben Schultz <bens4lsu@gmail.com>
2024-10-10 13:03:10 -0500

    import FoundationNetworking for linux

--- Sources/App/BlueskyClients/BlueskyAPIClient.swift
+++ Sources/App/BlueskyClients/BlueskyAPIClient.swift
@@ -7,6 +7,9 @@
 
 import Foundation
 import Logging
+#if os(Linux)
+import FoundationNetworking
+#endif
 
 public class BlueskyAPIClient {
     public let host: String
--- Sources/App/BlueskyClients/BlueskyClient.swift
+++ Sources/App/BlueskyClients/BlueskyClient.swift
@@ -7,6 +7,9 @@
 
 import Foundation
 import Logging
+#if os(Linux)
+import FoundationNetworking
+#endif
 
 public class BlueskyClient: BlueskyAPIClient {
     
