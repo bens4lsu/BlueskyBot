bef87a3469b6871a47e559ef17ec0d45c05fa718
Ben Schultz <bens4lsu@gmail.com>
2024-10-10 13:04:02 -0500

    import FoundationNetworking for linux

--- Sources/App/BlueskyModels/BlueskyAPIError.swift
+++ Sources/App/BlueskyModels/BlueskyAPIError.swift
@@ -6,6 +6,9 @@
 //
 
 import Foundation
+#if os(Linux)
+import FoundationNetworking
+#endif
 
 public enum BlueskyAPIError: LocalizedError, CustomStringConvertible {
     case invalidCode(response: HTTPURLResponse)
